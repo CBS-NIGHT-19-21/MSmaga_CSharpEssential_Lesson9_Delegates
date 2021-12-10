76bff3bc316a7fbe18fc699921033d685d9aa1c8
MSmaga <msmaga@protonmail.com>
2021-12-10 23:50:55 +0200

    Added MyPractice Delegates Rewrote Comments according to ECMA 334

---
+++ MyPractice1_Delegates/Classes/Account.cs
@@ -0,0 +1,39 @@
+﻿// Declare a delegate as a container for a method - reference for crossplatform of the algorithm.
+// A container for the method what gets string input parameter and returns no value.
+delegate void ClassDelegate(string message);
+
+class Account
+{
+    #region
+    // A delegate is a data structure that refers to one or more methods. 
+
+    // A delegate declaration defines a class that is derived from the class System.Delegate.A delegate
+    // instance encapsulates an invocation list, which is a list of one or more methods, each of which is referred
+    // to as a callable entity.
+    #endregion
+
+    // Instance the the delegate ClassDelegate as _myDelegate.
+    ClassDelegate? _myDelegate;
+
+    // Declare the public method ClassDelegate() for assigning
+    // this platform method into the instance of delegate ClassDelegate.
+    public void AddMethod(ClassDelegate showMessage)
+    {
+        _myDelegate += showMessage;
+    }
+    public void SubMethod(ClassDelegate showMessage)
+    {
+        if (_myDelegate != null)
+        {
+            _myDelegate  -= showMessage;
+        }
+        
+    }
+
+    // Call method placed in the instance of delegate ClassDelegate.
+    public void ShowMessage(string str)
+    {
+
+        _myDelegate!(str);
+    }
+}
--- MyPractice1_Delegates/Program.cs
+++ MyPractice1_Delegates/Program.cs
@@ -1,31 +1,51 @@
 ﻿namespace Delegates
 {
     class Program
-    {
-        // Declare delegates as methods - references.
+    {   // Declare this platform method for output of string message.
+        static void ShowWithEqual(string message)
+        {
+            Console.WriteLine(message + new string('=', 13));
+        }
+        static void ShowWithPlus(string message)
+        {
+            Console.WriteLine(message + new string('+', 13));
+        }
 
-        // For methods what gets no input parameters and returns no value.
-        delegate void Output();
-
-        // For methods what gets int input parameter and returns string value.
-        delegate string IntConvert(int intParameter);
-
-        // For methods what gets double input parameter and returns string value.
-        delegate string DoubleConvert(double doubleParameter);
-
-      
         static void Main(string[] args)
         {
-            Output output;
-            output = Show;
-            output.Invoke();
-        }
+            // Instance class Account.
+            Account account = new Account();
 
-        static void Show()
-        {
-            Console.WriteLine("Blah-Blah-Blah");
-        }
+            // Add this platform methods ShowWithEqual() and ShowWithPlus()
+            // to the delegate ClassDelegate as a field of the Account instance
+            // using public method of Account class - AddMethod().
+            for (int i = 0; i < 5; i++)
+            {
+                account.AddMethod(ShowWithEqual);
+                account.AddMethod(ShowWithPlus);
+            }
 
+            // Call all this platform methods ShowWithEqual() and ShowWithPlus()
+            // through method ShowMessage().
+            account.ShowMessage("Hi, there!");
+
+
+            Console.WriteLine(new string('_', 19));
+
+            // Sub this platform methods ShowWithEqual() 3 times
+            // from the delegate ClassDelegate as a field of the Account instance
+            // using public method of Account class - AddMethod().
+            for (int i = 0; i < 3; i++)
+            {
+                account.SubMethod(ShowWithEqual);
+            }
+
+            // Call the rest of this platform methods placed the in delegate ClassDelegate
+            // as a field of the Account instance.
+            account.ShowMessage("Hi, there!");
+            // Delay.
+            Console.ReadLine();
+        }
     }
 }
 
