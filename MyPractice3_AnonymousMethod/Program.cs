c3f679d1d6688aee6d4d7a54632fc42744317920
MSmaga <msmaga@protonmail.com>
2021-12-11 23:52:58 +0200

    Added Lamda operator practice

--- MyPractice3_AnonymousMethod/Program.cs
+++ MyPractice3_AnonymousMethod/Program.cs
@@ -5,22 +5,34 @@
         // Declare delegate type Delegate.
         public delegate double DelegateAnonMethod(double operand1, double operand2);
         public delegate int DelegateLambdaExp(int operand1, int operand2);
+        public delegate bool DelegateLambdaOper(float operand1, float operand2);
         static void Main(string[] args)
         {
             // Instance Delegate and assign anonymous method what doesn`t have its signature.
-            DelegateAnonMethod mD =new DelegateAnonMethod
+            DelegateAnonMethod mAnonMethod = new DelegateAnonMethod
             (delegate(double operand1, double operand2) { return operand1 * operand2; });
 
             // Instance Delegate and assign Lambda Expression what doesn`t have its signature.
-            DelegateLambdaExp mLambda = (int operand1, int operand2) => { return operand1 * operand2; };
+            DelegateLambdaExp mLambdaExp = (int operand1, int operand2) => { return operand1 * operand2; };
   
 
             // Call the anonymous method through a gelegate reference.
-            Console.WriteLine(mD(13.0, 19.0));
+            Console.WriteLine(mAnonMethod(13.0, 19.0));
 
             // Call the lambda expression through a gelegate reference.
-            Console.WriteLine(mLambda(13, 19));
+            Console.WriteLine(mLambdaExp(13, 19));
 
+            // Lambda operator has some lambda expressions.
+
+            // Instance Delegate and assign Lambda Expression what doesn`t have its signature.
+            DelegateLambdaOper mLambdaOper = (float operand1, float operand2) => 
+            {
+                float  buffer = operand1 * operand2;
+                return (buffer % 2  < 13); 
+            };
+
+            // Call the Lambda operator through a gelegate reference.
+            Console.WriteLine(mLambdaOper(13, 19));
 
             // Delay.
             Console.ReadLine();
